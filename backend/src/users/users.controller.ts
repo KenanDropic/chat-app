eaec8f5596b79ded30b6457eb0b229a846ee980b
Kenan Dropić <dropic5@hotmail.com>
2022-08-05 09:23:26 +0200

    find all users,find one user,create user

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.find();
+  }
+
+  @Get('/:id')
+  findOne(@Param('id') id: number): Promise<User> {
+    return this.usersService.findOne(id);
+  }
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RegisterUserDto } from 'src/auth/dto/index';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  async find() {
+    const users = await this.repo.find({});
+
+    if (!users) throw new NotFoundException('Users not found');
+
+    return users;
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.repo.findOne({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
+  }
+
+  async findOneByEmail(email: string): Promise<User> {
+    const user = await this.repo.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
+  }
+
+  create(args: RegisterUserDto): Promise<User> {
+    if (!args) throw new BadRequestException();
+
+    const user: User = this.repo.create(args);
+
+    return this.repo.save(user);
+  }
+}
