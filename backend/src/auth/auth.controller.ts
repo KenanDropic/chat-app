bad6fdd4fd69bb44749880c101306071cfb3c7df
Kenan Dropić <dropic5@hotmail.com>
2022-08-05 11:30:39 +0200

    login & signup

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { UsersService } from 'src/users/users.service';
+import { AuthService } from './auth.service';
+import { RegisterUserDto, LoginUserDto } from './dto/index';
+import { ResponseMessage } from './types/index';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
+
+  @Post('/register')
+  register(
+    @Res({ passthrough: true }) res: Response,
+    @Body() body: RegisterUserDto,
+  ): Promise<ResponseMessage> {
+    return this.authService.signup(res, body);
+  }
+
+  @Post('/login')
+  login(
+    @Res({ passthrough: true }) res: Response,
+    @Body() body: LoginUserDto,
+  ): Promise<ResponseMessage> {
+    return this.authService.signin(res, body);
+  }
+}
