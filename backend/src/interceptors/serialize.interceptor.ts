eecd040521bb62582678fa64a9dac7549440691e
Kenan Dropić <dropic5@hotmail.com>
2022-08-05 10:58:33 +0200

    serialize interceptor

---
+++ backend/src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    // running before the handler
+
+    return next.handle().pipe(
+      map((data: any) => {
+        // running before the response is sent out
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
