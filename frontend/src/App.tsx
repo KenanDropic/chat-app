f29d60573737aa3acc02f253d6c422570dddeb7e
Kenan Dropić <dropic5@hotmail.com>
2022-08-07 11:31:14 +0200

    react router & toast container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
-function App() {
-  return <h1>CHAT APP</h1>;
-}
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>CHAT APP</h1>
+      <ToastContainer
+        autoClose={4000}
+        draggable={false}
+        pauseOnHover={false}
+        pauseOnFocusLoss={false}
+      />
+    </>
+  );
+};
 
 export default App;
