90135cdaf3f7dce22b702ca081cccd73f6feeae7
Kenan Dropić <dropic5@hotmail.com>
2022-08-09 08:39:21 +0200

    cookie parser & cors configuration

---
+++ backend/src/utils/allowedOrigins.ts
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'http://127.0.0.1:5000',
+    'http://localhost:3000',
+    'http://localhost:5000',
+  ];
+  
+  export default allowedOrigins;
+  
---
+++ backend/src/utils/corsOptions.ts
@@ -0,0 +1,15 @@
+import allowedOrigins from './allowedOrigins';
+
+const corsOptions = {
+  credentials: true,
+  origin: (origin: any, callback: any) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+export default corsOptions;
