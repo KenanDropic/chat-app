65f7403e489269c6a0a1e943790b618026b9f373
Kenan Dropić <dropic5@hotmail.com>
2022-08-10 09:08:09 +0200

    private component

---
+++ frontend/src/components/Private.tsx
@@ -0,0 +1,26 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useGetMeQuery } from "../features/auth/authApiSlice";
+import { Roles } from "./interfaces/interfaces";
+import Spinner from "./Spinner";
+
+// @ts-ignore comment
+const Private: React.FC<Roles> = ({ allowedRoles }) => {
+  const location = useLocation();
+  const { data, isLoading } = useGetMeQuery();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (!isLoading) {
+    return data && allowedRoles.includes(data?.role) ? (
+      <Outlet />
+    ) : data ? (
+      <Navigate to="/unauthorized" state={{ from: location }} replace />
+    ) : (
+      <Navigate to="/login" state={{ from: location }} replace />
+    );
+  }
+};
+
+export default Private;
